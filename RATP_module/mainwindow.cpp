690020646c1bc68f292c9b867a71e23166d913e7
Florian.F <francoisflorian4@gmail.com>
2019-04-17 16:58:00 +0200

    update RATP module

--- RATP_module/mainwindow.cpp
+++ RATP_module/mainwindow.cpp
@@ -6,9 +6,36 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+    connect(manager, SIGNAL(authenticationRequired(QNetworkReply*,QAuthenticator*)), this, SLOT(onAuthenticationRequestSlot(QNetworkReply*,QAuthenticator*)));
+    connect(manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(replyFinished(QNetworkReply*)));
+
+    request = new QNetworkRequest () ;
+    request->setHeader(QNetworkRequest::ContentTypeHeader,"application/x-www-form-urlencoded");
+    request->setUrl(QUrl("https://api-lab-trall-stif.opendata.stif.info/service/tr-globale-stif/estimated-timetable"));
+    userdata = new QByteArray ("40cd855f9c40cffde532f9409f94b9f3e36648cf6bec24d347e963d5");
+
+    manager->post(*request,*userdata);
+    manager->get(*request);
 }
 
 MainWindow::~MainWindow()
 {
+    delete manager;
     delete ui;
+}
+
+void MainWindow::onAuthenticationRequestSlot(QNetworkReply *_rep, QAuthenticator *authenticator)
+{
+    qDebug() << "auth" << _rep->error();
+    authenticator->setUser(username);
+    authenticator->setPassword(password);
+}
+
+void MainWindow::replyFinished(QNetworkReply* data)
+{
+    qDebug() << "hello" << data->error();
+    qDebug() << "hello" << data->readAll();
+
 }
--- RATP_module/mainwindow.h
+++ RATP_module/mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkReply>
+#include <QJsonDocument>
+#include <QAuthenticator>
+
 
 namespace Ui {
 class MainWindow;
@@ -14,7 +19,17 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    QString username = "francoisflorian4@gmail.com";
+    QString password = "floflo123";
+    QString keyapi = "40cd855f9c40cffde532f9409f94b9f3e36648cf6bec24d347e963d5";
+    QNetworkAccessManager *manager;
+    QNetworkRequest *request;
+    QNetworkReply *reply;
+    QByteArray *userdata;
 
+public slots:
+    void replyFinished(QNetworkReply *_rep);
+    void onAuthenticationRequestSlot(QNetworkReply *reply, QAuthenticator *authenticator);
 private:
     Ui::MainWindow *ui;
 };
