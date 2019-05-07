75a5ff6cc9d3332616d71725e0737594fa6d012a
Haïssam <gadhaissam@gmail.com>
2019-05-07 16:45:43 +0200

    Update traducteur v2

--- src/dialogmeteo.cpp
+++ src/dialogmeteo.cpp
@@ -34,17 +34,20 @@ Dialog::Dialog(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    ui->labelDate->setText(QTime::currentTime().toString("hh:mm"));
-    ui->labelDate->setText(QDate::currentDate().toString());
-    ui->labelDate->setText(QDateTime::currentDateTime().toString(tr("ddd d MMMM  hh:mm ")));
-
-
     m_meteo = new meteo;
     p_pollution = new Pollution;
     i_indice = new IndiceUV;
     pm_prevision=new Prevision;
-    l_lang = new Dialogtraduction(this);
+    QPixmap uvprotection;
 
+    //ui->labelDate->setText(QTime::currentTime().toString("hh:mm"));
+    //ui->labelDate->setText(QDate::currentDate().toString());
+    ui->labelDate->setText(QDateTime::currentDateTime().toString(("ddd d MMMM  hh:mm ")));
+
+    setvignette();
+
+    uvprotection.load(":/Icons_meteo/protectionUV.png");
+    ui->label_UVprotection->setPixmap(uvprotection);
 
     connect(m_meteo,SIGNAL(received()),this,SLOT(printHashmeteo()));
     connect(p_pollution,SIGNAL(received()),this,SLOT(printHashpollution()));
@@ -54,14 +57,6 @@ Dialog::Dialog(QWidget *parent) :
     connect(p_pollution,SIGNAL(received()),this,SLOT(AQI()));
     connect(p_pollution,SIGNAL(received()),this,SLOT(Icon()));
     connect(ui->pushButton_close,SIGNAL(clicked()),this,SLOT(close()));
-
-
-    setvignette();
-
-    QPixmap uvprotection;
-    uvprotection.load(":/Icons_meteo/protectionUV.png");
-    ui->label_UVprotection->setPixmap(uvprotection);
-
 
 }
 
@@ -288,37 +283,14 @@ void Dialog::Icon()                                        //affichage de l'icon
 void Dialog::loadlanguage(QString lang)
 {
 
-    qDebug() << "Iciiiiiiiii!!!!!";
+    qDebug() << "Météo";
 
     qDebug() << lang;
     QTranslator translator;
 
-    if(lang == "Anglais")
-    {
-        qDebug()<< translator.load(":/Traduction/src_en.qm");
-        qApp->installTranslator(&translator);
-        ui->retranslateUi(this);
-    }
-
-    else if(lang == "Français")
-    {
-        qDebug()<< translator.load(":/Traduction/src_fr.qm");
-        qApp->installTranslator(&translator);
-        ui->retranslateUi(this);
-    }
-
-    else if(lang == "Arabe")
-    {
-        qDebug()<< translator.load(":/Traduction/src_ar.qm");
-        qApp->installTranslator(&translator);
-        ui->retranslateUi(this);
-    }
-
-
-
-    l_lang->hide();
-
-
+    translator.load((QString(":/Traduction/src_%1.qm").arg(lang)));
+    qApp->installTranslator(&translator);
+    ui->retranslateUi(this);
 }
 
 
