5f2689b8be34a32ca3f2b1de029d8872eac9ff07
jennifer <jnnfr.mrtn@gmx.fr>
2019-05-09 11:17:00 +0200

    modifs

--- src/dialogvigicrues.cpp
+++ src/dialogvigicrues.cpp
@@ -19,10 +19,10 @@ DialogVigicrues::DialogVigicrues(QWidget *parent) :
 
     // Titre du Graph
     ui->widgetGraph->plotLayout()->insertRow(0);
-    ui->widgetGraph->plotLayout()->addElement(0,0, new QCPTextElement(ui->widgetGraph, "Hauteurs et débits des dernières 24h"));
+    ui->widgetGraph->plotLayout()->addElement(0,0, new QCPTextElement(ui->widgetGraph, "Hauteurs et débits"));
 
     //Ajout date
-    ui->labelDate->setText((QDateTime::currentDateTime().toString("dddd dd MMMM yyyy")));
+    //ui->labelDate->setText((QDateTime::currentDateTime().toString("dddd dd MMMM yyyy")));
 
     h_hauteur = new VigiCruesHauteur;
     connect(h_hauteur,SIGNAL(received()),this,SLOT(printHashhauteur()));
