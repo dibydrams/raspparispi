8648cde7039af23a5c921c8f9c177ea64fa9dfe1
Haiissam <gadhaissam@gmail.com>
2019-06-25 09:56:09 +0200

    Mise à jours api pollution

--- src/pollution.cpp
+++ src/pollution.cpp
@@ -18,10 +18,15 @@ Pollution::Pollution()
 
 void Pollution::setRequest()
 {
-
-    manager = new QNetworkAccessManager(this);
+    //API weatherbit
+    /*manager = new QNetworkAccessManager(this);
     QUrl url("http://api.weatherbit.io/v2.0/current/airquality?lat=48.86&lon=2.35&key=9b80e0427ca94ad39b83354f3f8e2b64");
-    //https://api.waqi.info/feed/paris/?token=a74334edb92ad4b8ba053a6fa13c2c7ca7fb6f8d
+    request.setUrl(url);
+    reply = manager->get(request);*/
+
+    //API waqi
+    manager = new QNetworkAccessManager(this);
+    QUrl url("https://api.waqi.info/feed/paris/?token=a74334edb92ad4b8ba053a6fa13c2c7ca7fb6f8d");
     request.setUrl(url);
     reply = manager->get(request);
 
@@ -46,8 +51,8 @@ QVariantHash Pollution::readJson()
     QHash <QString, QVariant> hash;
 
 
-    /*Récupérer les valeurs des polluants (array)*/
-    QJsonArray tableauJSON;
+    /*Récupérer les valeurs des polluants (array) WEATHERBIT*/
+    /*QJsonArray tableauJSON;
     tableauJSON = obj["data"].toArray();
 
     QJsonObject obj2 = tableauJSON[0].toObject();
@@ -58,8 +63,35 @@ QVariantHash Pollution::readJson()
     hash["NO2"]=(obj2.value("no2").toDouble());
     hash["PM25"]=(obj2.value("pm25").toDouble());
     hash["PM10"]=(obj2.value("pm10").toDouble());
-    hash["CO"]=(obj2.value("co").toDouble());
+    hash["CO"]=(obj2.value("co").toDouble());*/
 
+    /*Récupérer les valeurs des polluants WAQI*/
+
+      QJsonObject obj2 = obj["data"].toObject();
+      hash["AQI"]=(obj2.value("aqi").toDouble());
+
+      QJsonObject obj3 = obj2["iaqi"].toObject();
+
+      QJsonObject obj4 = obj3["o3"].toObject();
+      hash["O3"]=(obj4.value("v").toDouble());
+
+      QJsonObject obj5 = obj3["so2"].toObject();
+      hash["SO2"]=(obj5.value("v").toDouble());
+
+      QJsonObject obj6 = obj3["no2"].toObject();
+      hash["NO2"]=(obj6.value("v").toDouble());
+
+      QJsonObject obj7 = obj3["pm25"].toObject();
+      hash["PM25"]=(obj7.value("v").toDouble());
+
+      QJsonObject obj8 = obj3["pm10"].toObject();
+      hash["PM10"]=(obj8.value("v").toDouble());
+
+      QJsonObject obj9 = obj3["co"].toObject();
+      hash["CO"]=(obj9.value("v").toDouble());
+
+
+    qDebug() << hash;
 
     return hash;
 
