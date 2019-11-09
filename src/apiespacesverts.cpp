44cfca39d6b41633b78359e997c72d6093db2731
luthomasly <luthomasly@gmail.com>
2019-11-09 22:41:12 +0100

    mise à jour du lien, en cours de resolution

--- src/apiespacesverts.cpp
+++ src/apiespacesverts.cpp
@@ -96,7 +96,6 @@ void ApiEspacesVerts::firstCall()
     lonCentre = QString::number(WidgetMap::centreLongitude, 'g', 13);
 
     //En attente, car mise à jour de la nouvelle URL - 2019/10/25 - tly
-    //QUrl url("https://opendata.paris.fr/api/records/1.0/search/?dataset=espaces_verts&rows=-1&facet=type_ev&facet=categorie&facet=adresse_codepostal&facet=presence_cloture&facet=ouvert_ferme&geofilter.distance="+latCentre+"%2C"+lonCentre+"%2C"+rayon);
     QUrl url("https://opendata.paris.fr/api/records/1.0/search/?dataset=espaces_verts&rows=-1&facet=type_ev&facet=categorie&facet=adresse_codepostal&facet=presence_cloture&facet=ouvert_ferme&geofilter.distance="+latCentre+"%2C"+lonCentre+"%2C"+rayon);
 
     //qDebug() << url;
